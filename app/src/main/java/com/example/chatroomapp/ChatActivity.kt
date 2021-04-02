bb88fd3ab227bd23ced2ce0bb90ed32074489bb9
ajallen <ajallen10@gmail.com>
2021-04-01 20:08:00 -0600

    Successfully wrote to RealtimeDatabase, got rid of bottom navigation view.

---
+++ app/src/main/java/com/example/chatroomapp/ChatActivity.kt
@@ -0,0 +1,27 @@
+package com.example.chatroomapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.floatingactionbutton.FloatingActionButton
+import com.google.android.material.snackbar.Snackbar
+import com.google.firebase.database.FirebaseDatabase
+
+
+class ChatActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_chat)
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+        findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
+            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+
+        val database = FirebaseDatabase.getInstance()
+        val myRef = database.getReference("message")
+
+        myRef.setValue("Hello, World!")
+    }
+}
