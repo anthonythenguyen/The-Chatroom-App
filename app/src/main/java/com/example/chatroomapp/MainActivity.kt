5fc346d81c620b87a67096be4979d076ad8ccf29
ajallen <ajallen10@gmail.com>
2021-04-12 16:42:59 -0600

    Fixed small error, figuring out how to pull data without clicking a button

--- app/src/main/java/com/example/chatroomapp/MainActivity.kt
+++ app/src/main/java/com/example/chatroomapp/MainActivity.kt
@@ -4,8 +4,10 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.ListView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import com.google.android.material.floatingactionbutton.FloatingActionButton
@@ -19,9 +21,7 @@ class MainActivity : AppCompatActivity() {
     private lateinit var auth: FirebaseAuth
     var database = FirebaseDatabase.getInstance().getReference("users")
     var username = ""
-
     private var mTopToolbar: Toolbar? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,43 +29,49 @@ class MainActivity : AppCompatActivity() {
         setSupportActionBar(findViewById(R.id.toolbar))
 
         auth = FirebaseAuth.getInstance()
-        var database = FirebaseDatabase.getInstance()
+        if(auth.currentUser == null){
+            val intent = Intent(this, SignInActivity::class.java)
+            startActivity(intent)
+        }
 
-        var tempChat = findViewById<Button>(R.id.tempChat)
-        var myList = findViewById(R.id.myListView) as ListView
-        var listOfCurrentConvos = arrayListOf<String>("Examples", "Jackson Holt", "unicorn8343")
+        database.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(dataSnapshot: DataSnapshot) {
+                var snap = dataSnapshot.children
+                for (i in snap) {
+                    val data: String? = i.key
+                    for (j in i.children) {
+                        if (data != null && j.value == auth.currentUser?.uid) {
+                            username = i.key.toString()
+                        }
+                    }
+                }
+            }
 
+            override fun onCancelled(databaseError: DatabaseError) {}
+        })
 
-
-
-
-
+        var listOfCurrentConvos = arrayListOf<String>()
         var ref = database.child(username).child("conversations")
+
         ref.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(dataSnapshot: DataSnapshot) {
                 var snap = dataSnapshot.children
 
                 for (i in snap) {
                     val data: String? = i.key
-
                     listOfCurrentConvos.add(data.toString())
+                }
+
+                var myList = findViewById(R.id.myListView) as ListView
+                var listAdapter: ArrayAdapter<String> =
+                    ArrayAdapter(this@MainActivity, android.R.layout.simple_list_item_1, listOfCurrentConvos)
+                myList.adapter = listAdapter
             }
 
             override fun onCancelled(databaseError: DatabaseError) {}
         })
 
 
-
-
-
-
-        tempChat.setOnClickListener {
-            startActivity(Intent(this, ChatActivity::class.java))
-        }
-        if(auth.currentUser == null){
-            val intent = Intent(this, SignInActivity::class.java)
-            startActivity(intent)
-        }
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
 //            Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
